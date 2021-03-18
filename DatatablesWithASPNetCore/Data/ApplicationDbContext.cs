b4638f70e3b80b211ca8bad47b15cededebff44b
Enrico Rossini <enrico.rossini.uk@live.com>
2021-03-18 17:25:32 +0000

    First run of Datatables.net

---
+++ DatatablesWithASPNetCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DatatablesWithASPNetCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatatablesWithASPNetCore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ DatatablesWithASPNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DatatablesWithASPNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
