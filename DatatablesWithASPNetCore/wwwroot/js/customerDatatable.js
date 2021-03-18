d6da10f60ac7ce02cd0580a497a97188eda4ea0b
Enrico Rossini <enrico.rossini.uk@live.com>
2021-03-18 17:47:44 +0000

    Update customerDatabase.js and api

--- DatatablesWithASPNetCore/wwwroot/js/customerDatatable.js
+++ DatatablesWithASPNetCore/wwwroot/js/customerDatatable.js
@@ -1,4 +1,30 @@
 ﻿$(document).ready(function () {
-    $('#customerDatatable').dataTable({
+    $("#customerDatatable").DataTable({
+        "processing": true,
+        "serverSide": true,
+        "filter": true,
+        "ajax": {
+            "url": "/api/customer",
+            "type": "POST",
+            "datatype": "json"
+        },
+        "columnDefs": [{
+            "targets": [0],
+            "visible": false,
+            "searchable": false
+        }],
+        "columns": [
+            { "data": "id", "name": "Id", "autoWidth": true },
+            { "data": "firstName", "name": "First Name", "autoWidth": true },
+            { "data": "lastName", "name": "Last Name", "autoWidth": true },
+            { "data": "contact", "name": "Country", "autoWidth": true },
+            { "data": "email", "name": "Email", "autoWidth": true },
+            { "data": "dateOfBirth", "name": "Date Of Birth", "autoWidth": true },
+            {
+                "render": function (data, row) {
+                    return "<a href='#' class='btn btn-danger' onclick=DeleteCustomer('" + row.id + "'); >Delete</a>";
+                }
+            },
+        ]
     });
 });
